b4cf5f694b48b50f32f41030523e5e653f3d6469
Sergey <denisenko.sa@ffx.media>
2019-02-14 08:44:43 +0300

    Домашнее задание. Урок 1

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Задание 1
+alert("Задание 1");
+var temperatureCelsius = prompt("Введите температуру в Цельсиях");
+var temperatureFahrenheit = temperatureCelsius * 1.8 + 32;
+alert("Температура по Фарингейту " + temperatureFahrenheit);
+
+// Задание 2
+alert("Задание 2");
+var admin, name;
+name = "Василий";
+admin = name;
+alert("admin: " + admin);
+
+//Задание 3
+alert("Задание 3");
+alert("Сложение 1000 + '108' (сложение строки с числом)")
+//При сложение строки с числом получается склеивание значений.
+alert("При сложение строки с числом получается склеивание значений. В итоге получаем 1000108");
